d01cf15b01b45568ee5058a3d8b08530ad784c1c
Jeet Dholakia <onemediasoft@gmail.com>
2022-11-26 13:47:09 +0530

    Unfollows user

--- src/routers/follow.js
+++ src/routers/follow.js
@@ -29,16 +29,64 @@ router.put("/:id/follow", auth, async (req, res) => {
                 })
             }
         } catch (err) {
-            res.status(400).json({
-                statusCode: 400,
+            res.status(500).json({
+                statusCode: 500,
                 status: "Error",
                 data: null,
                 message: err.message
             })
         }
     } else {
-        res.status(403).json("you cant follow yourself");
+        res.status(403).json({
+            statusCode: 403,
+            status: "Error",
+            data: null,
+            message: "You cannot follow yourself"
+        })
     }
 });
+
+router.put("/:id/unfollow", auth, async (req, res) => {
+    const requestingUserId = req.params.id
+    const currentUserId = req.body.user.id
+
+    if (currentUserId !== requestingUserId) {
+      try {
+        const user = await User.findById(requestingUserId);
+        const currentUser = await User.findById(currentUserId);
+        if (user.followers.includes(currentUserId)) {
+          await user.updateOne({ $pull: { followers: currentUserId } });
+          await currentUser.updateOne({ $pull: { followings: requestingUserId } });
+            res.status(200).json({
+                statusCode: 200,
+                status: "Success",
+                data: user,
+                message: "User unfollowed successfully"
+            })
+        } else {
+            res.status(403).json({
+                statusCode: 403,
+                status: "Error",
+                data: null,
+                message: "You don't follow this user"
+            })
+        }
+      } catch (err) {
+          res.status(500).json({
+              statusCode: 500,
+              status: "Error",
+              data: null,
+              message: err.message
+          })
+      }
+    } else {
+        res.status(403).json({
+            statusCode: 403,
+            status: "Error",
+            data: null,
+            message: "You cannot unfollow yourself"
+        })
+    }
+  });
 
 module.exports = router
